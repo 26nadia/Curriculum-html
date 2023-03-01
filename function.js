47c1b83ae94f655befa32d1c0d3a771692e3e41e
Nadia Cruz <nadiajoanacruz@hotmail.com>
2023-03-01 17:12:55 +0100

    mi primer commit

---
+++ function.js
@@ -0,0 +1,7 @@
+document.getElementById('boton_color').addEventListened('clik', function() {
+    document.body.style.backgroundcolor = 'pink';
+});
+
+document.getElementById('boton_defaul').addEventListened('clik', function() {
+    document.body.style.backgroundcolor = 'grey';
+});
